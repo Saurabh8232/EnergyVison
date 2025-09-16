4bb657a6ba18c1387625057abbb9b7ae62fe99fd
Saurabh Yadav <sy962965@gmail.com>
2025-09-16 13:30:25 +0000

    now check all file related like this problem and updated vision to vison

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,14 +3,14 @@ import { initializeApp, getApp, getApps } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  "projectId": "energyvision-l60un",
+  "projectId": "energyvison-l60un",
   "appId": "1:493264983763:web:82868beb596aa1887a2626",
   "storageBucket": "energyvision-l60un.firebasestorage.app",
   "apiKey": "AIzaSyCAZWHLnyPDARl0Tlm8fhTXS2asEMUYVr0",
   "authDomain": "energyvision-l60un.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "493264983763",
-  "databaseURL": "https://energy-vison-l60un-default-rtdb.firebaseio.com/"
+  "databaseURL": "https://energyvison-l60un-default-rtdb.firebaseio.com/"
 };
 
 // Initialize Firebase
@@ -18,5 +18,6 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const database = getDatabase(app);
 
 export { app, database };
+
 
 
